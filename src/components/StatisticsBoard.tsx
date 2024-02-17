179c7d4ab42b72339ee7f217f59607a970f0f193
Emilia Hansson <eh222yn@student.lnu.se>
2024-02-17 18:53:42 +0100

    Make code more readable

--- src/components/StatisticsBoard.tsx
+++ src/components/StatisticsBoard.tsx
@@ -6,55 +6,60 @@ import { getAsteroidsByDate } from '../features/asteroids/asteroidSlice'
 import AsteroidAnalyzer from '../utils/AsteroidAnalyzer'
 import CircularProgress from '@mui/material/CircularProgress'
 
+type Asteroid = {
+  potentiallyHazardous: string
+  distanceFromEarth: string
+  orbiting: string
+  estimatedDiameter: string
+  relativeVelocity: string
+  name: string
+}
+
 const StatisticsBoard = () => {
   const dispatch: AppDispatch = useDispatch()
-  const { asteroidData, isPending } = useSelector(
-    (state: RootState) => state.asteroids,
-  )
 
-  const [potentiallyHazard, setPotentiallyHazard] = useState<boolean>(false)
-  const [distanceFromEarth, setDistanceFromEarth] = useState<string>('')
-  const [orbiting, setOrbiting] = useState<string>('')
-  const [estimatedDiameter, setEstimatedDiameter] = useState<string>('')
-  const [relativeVelocity, setRelativeVelocity] = useState<string>('')
-  const [name, setName] = useState<string>('')
+  const { asteroidData, isPending } = useSelector((state: RootState) => state.asteroids)
+  const [asteroidOfTheDay, setAsterodOfTheDay] = useState<Asteroid>({
+    potentiallyHazardous: '',
+    distanceFromEarth: '',
+    orbiting: '',
+    estimatedDiameter: '',
+    relativeVelocity: '',
+    name: '',
+  })
 
   useEffect(() => {
-    var todayDate = new Date().toISOString().slice(0, 10)
+    const todaysDate = new Date().toISOString().slice(0, 10)
 
-    dispatch(getAsteroidsByDate({ startDate: todayDate, endDate: todayDate }))
+    dispatch(getAsteroidsByDate({ startDate: todaysDate, endDate: todaysDate }))
   }, [])
 
   useEffect(() => {
     if (asteroidData) {
       if (asteroidData.near_earth_objects) {
-        const asteroids = Object.values(asteroidData.near_earth_objects).reduce(
-          (acc, curr) => acc.concat(curr),
-          [],
-        )
+        const asteroids = Object.values(asteroidData.near_earth_objects).reduce((acc, curr) => acc.concat(curr), [])
         const analyzer = new AsteroidAnalyzer(asteroids)
 
         const closestAsteroid: any = analyzer.getClosestAsteroid()
 
-        setPotentiallyHazard(closestAsteroid.is_potentially_hazardous_asteroid)
-        setDistanceFromEarth(
-          Math.round(
-            parseFloat(
-              closestAsteroid.close_approach_data[0].miss_distance.kilometers,
-            ),
-          ).toFixed(0),
-        )
-        setOrbiting(closestAsteroid.close_approach_data[0].orbiting_body)
-        setEstimatedDiameter(
-          `${parseFloat(closestAsteroid.estimated_diameter.meters.estimated_diameter_min).toFixed(2)} - ${parseFloat(closestAsteroid.estimated_diameter.meters.estimated_diameter_max).toFixed(2)}`,
-        )
-        setRelativeVelocity(
-          parseFloat(
-            closestAsteroid.close_approach_data[0].relative_velocity
-              .kilometers_per_second,
-          ).toFixed(0),
-        )
-        setName(closestAsteroid.name)
+        const name = closestAsteroid.name
+        const potentiallyHazardous = closestAsteroid.is_potentially_hazardous_asteroid ? 'Yes' : 'No'
+        const distanceFromEarth = parseFloat(closestAsteroid.close_approach_data[0].miss_distance.kilometers).toFixed(0)
+        const orbiting = closestAsteroid.close_approach_data[0].orbiting_body
+        const diameterMin = parseFloat(closestAsteroid.estimated_diameter.meters.estimated_diameter_min).toFixed(2)
+        const diameterMax = parseFloat(closestAsteroid.estimated_diameter.meters.estimated_diameter_max).toFixed(2)
+        const relativeVelocity = parseFloat(
+          closestAsteroid.close_approach_data[0].relative_velocity.kilometers_per_second,
+        ).toFixed(0)
+
+        setAsterodOfTheDay({
+          potentiallyHazardous,
+          distanceFromEarth,
+          orbiting,
+          estimatedDiameter: `${diameterMin} - ${diameterMax}`,
+          relativeVelocity,
+          name,
+        })
       }
     }
   }, [asteroidData])
@@ -64,21 +69,12 @@ const StatisticsBoard = () => {
       {!isPending ? (
         <div className='flex flex-col space-y-5'>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
-            <StatisticsCard header='Name' stats={name} />
-            <StatisticsCard
-              header='Distance from Earth (km)'
-              stats={distanceFromEarth}
-            />
-            <StatisticsCard header='Orbiting' stats={orbiting} />
-            <StatisticsCard header='Diameter (m)' stats={estimatedDiameter} />
-            <StatisticsCard
-              header='Relative velocity (km/s)'
-              stats={relativeVelocity}
-            />
-            <StatisticsCard
-              header='Potentially hazardous'
-              stats={potentiallyHazard ? 'Yes' : 'No'}
-            />
+            <StatisticsCard header='Name' stats={asteroidOfTheDay.name} />
+            <StatisticsCard header='Distance from Earth (km)' stats={asteroidOfTheDay.distanceFromEarth} />
+            <StatisticsCard header='Orbiting' stats={asteroidOfTheDay.orbiting} />
+            <StatisticsCard header='Diameter (m)' stats={asteroidOfTheDay.estimatedDiameter} />
+            <StatisticsCard header='Relative velocity (km/s)' stats={asteroidOfTheDay.relativeVelocity} />
+            <StatisticsCard header='Potentially hazardous' stats={asteroidOfTheDay.potentiallyHazardous} />
           </div>
         </div>
       ) : (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,8 @@ const Home = () => {
     <>
       <div className='flex flex-col h-full overflow-scroll w-full items-center py-8 sm:py-20 md:py-20 lg:py-20'>
         <div className='text-white flex flex-col items-center pl-5 pr-5'>
-          <p className='text-3xl lg:text-5xl md:text-5xl sm:text-5xl'>
-            Hello, I'm an Asteroid ☄️
-          </p>
-          <p className='text-sm lg:text-3xl md:text-3xl sm:text-2xl mt-3'>
+          <p className='text-3xl lg:text-5xl md:text-5xl sm:text-5xl'>Hello, I'm an Asteroid ☄️</p>
+          <p className='text-md lg:text-3xl md:text-3xl sm:text-2xl mt-3'>
             Today is a great day, I'm at my closest to Earth.
             <br></br>
             If you are lucky, you might spot me 👋

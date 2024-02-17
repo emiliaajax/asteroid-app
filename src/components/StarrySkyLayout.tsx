f17384377c6544efb9f3769a284dbea5511d2519
Emilia Hansson <eh222yn@student.lnu.se>
2024-02-17 15:24:16 +0100

    Add code comments

--- src/components/StarrySkyLayout.tsx
+++ src/components/StarrySkyLayout.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from 'react'
 import Star from '../utils/Star'
 
-const NUM_STARS: number = 400
-const NUM_COLORS: number = 3
+const NUM_STARS = 400
+const NUM_COLORS = 3
 
 type Props = {
   children: React.ReactNode
@@ -13,78 +13,95 @@ const StarrySkyLayout = ({ children }: Props) => {
   const animationRef = useRef<number>()
 
   useEffect(() => {
-    if (!canvasRef.current) return
-
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-
-    if (!context) return
-
-    function randomColor(): string {
+    /**
+     * Generates a random color from a predefined set of colors.
+     *
+     * @returns Random color.
+     */
+    const randomColor = (): string => {
       const arrColors = ['#ffffff', '#ffecd3', '#bfcfff']
       return arrColors[Math.floor(Math.random() * NUM_COLORS)]
     }
 
-    let canvasWidth: number = window.innerWidth
-    let canvasHeight: number = window.innerHeight
-
+    /**
+     * Resizes the canvas to match the window size.
+     */
     const resizeCanvas = () => {
-      canvasWidth = canvas.width = window.innerWidth
-      canvasHeight = canvas.height = window.innerHeight
+      if (canvasRef.current) {
+        const canvas = canvasRef.current
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+      }
     }
 
-    // Initialize canvas size
-    resizeCanvas()
-
-    window.addEventListener('resize', resizeCanvas)
-
-    const stars: Star[] = []
-
-    for (let i = 0; i < NUM_STARS; i++) {
-      const randX = Math.floor(Math.random() * canvasWidth) + 1
-      const randY = Math.floor(Math.random() * canvasHeight) + 1
-      const randR = Math.random() * 1.7 + 0.5
-
-      const star = new Star({
-        x: randX,
-        y: randY,
-        r: randR,
-        color: randomColor(),
-      })
-
-      stars.push(star)
-    }
-
-    // Function to update the stars' properties
-    function update() {
+    /**
+     * Updates the positions of stars.
+     *
+     * @param stars - Array of stars to update.
+     */
+    const updateStars = (stars: Star[]) => {
       for (let i = 0; i < stars.length; i++) {
         stars[i].update()
       }
     }
 
-    // Function to animate the stars
-    function animate() {
-      update()
-
+    /**
+     * Renders stars on the canvas.
+     * 
+     * @param context - Canvas rendering context.
+     * @param stars - Array of stars to render.
+     */
+    const renderStars = (
+      context: CanvasRenderingContext2D | null,
+      stars: Star[],
+    ) => {
       if (context) {
+        const canvasWidth = window.innerWidth
+        const canvasHeight = window.innerHeight
         context.clearRect(0, 0, canvasWidth, canvasHeight)
 
         for (let i = 0; i < stars.length; i++) {
           stars[i].render(context)
         }
       }
-
-      animationRef.current = requestAnimationFrame(animate) // Request next animation frame
     }
 
+    /**
+     * Animates the starry sky by updating and rendering stars.
+     */
+    const animate = () => {
+      const canvas = canvasRef.current
+      const context = canvas?.getContext('2d')
+      if (!canvas || !context) return
+
+      updateStars(stars)
+      renderStars(context, stars)
+      animationRef.current = requestAnimationFrame(animate)
+    }
+
+    // Create initial stars
+    const stars: Star[] = Array.from({ length: NUM_STARS }, () => {
+      const randX = Math.floor(Math.random() * window.innerWidth) + 1
+      const randY = Math.floor(Math.random() * window.innerHeight) + 1
+      const randR = Math.random() * 1.7 + 0.5
+
+      return new Star({
+        x: randX,
+        y: randY,
+        r: randR,
+        color: randomColor(),
+      })
+    })
+
+    // Initialize canvas and animation
+    resizeCanvas()
+    window.addEventListener('resize', resizeCanvas)
     animate()
 
-    // Clean-up function
+    // Clean up event listeners and animation frame
     return () => {
-      if (animationRef.current) {
-        cancelAnimationFrame(animationRef.current) // Cancel animation frame when component unmounts
-      }
-      window.removeEventListener('resize', resizeCanvas) // Remove resize listener
+      if (animationRef.current) cancelAnimationFrame(animationRef.current)
+      window.removeEventListener('resize', resizeCanvas)
     }
   }, [])
 

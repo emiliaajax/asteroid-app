a8eb694047df50f7823226f0e405b75d4388b5db
Emilia Hansson <eh222yn@student.lnu.se>
2024-02-17 17:28:45 +0100

    Fix statistic board for small screens

--- src/components/StatisticsBoard.tsx
+++ src/components/StatisticsBoard.tsx
@@ -62,7 +62,7 @@ const StatisticsBoard = () => {
     <>
       {!isPending ? (
         <div className='flex flex-col space-y-5'>
-          <div className='grid grid-cols-1 sm:grid-cols-3 gap-5'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
             <StatisticsCard header='Name' stats={name} />
             <StatisticsCard
               header='Distance from Earth (km)'
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import StatisticsBoard from '../components/StatisticsBoard'
 const Home = () => {
   return (
     <>
-      <div className='flex flex-col h-full overflow-scroll w-full items-center py-8 md:py-20 lg:py-20'>
+      <div className='flex flex-col h-full overflow-scroll w-full items-center py-8 sm:py-20 md:py-20 lg:py-20'>
         <div className='text-white flex flex-col items-center pl-5 pr-5'>
-          <p className='text-3xl lg:text-5xl md:text-3xl'>
+          <p className='text-3xl lg:text-5xl md:text-5xl sm:text-5xl'>
             Hello, I'm an Asteroid ☄️
           </p>
-          <p className='text-sm lg:text-3xl md:text-xl mt-3'>
+          <p className='text-sm lg:text-3xl md:text-3xl sm:text-2xl mt-3'>
             Today is a great day, I'm at my closest to Earth.
             <br></br>
             If you are lucky, you might spot me 👋
